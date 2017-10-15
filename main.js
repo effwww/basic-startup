fb9bde25203ec97e266250e4184cf6ec4173013b
John Liang <jliang6@hotmail.com>
2017-10-14 22:17:25 -0400

    Tutorial 6: Properly Setup jQuery and jQuery-UI with a working jQuery-UI widget working - Slider

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 "use strict";
 
 requirejs.config({
-    // baseUrl: "/features",
-    // waitSeconds: 10,
     paths: {
-        // jquery: "/node_modules/jquery/dist/jquery.min"
+        "jquery": "/node_modules/jquery/dist/jquery",
+        "jquery-ui": "/node_modules/jquery-ui-bundle/jquery-ui"
     },
     shim: {
-        /*
-        jquery: {
-            exports: '$'
-        }
-        */
+        // "jquery-ui": ['jquery_']
     }
 });
 
-
-require(['jquery'], function() {
+require(['jquery-ui'], function() {
     console.log("require function is being called.");
+    $("#slider").slider();
     // $('#EOB').before("<p id='mainRequire'>main.js require function</p>");
 });
+
+/*
+<script src="/node_modules/jquery/dist/jquery.js"></script>
+<script src="/node_modules/jquery-ui-bundle/jquery-ui.js"></script>
+<script src="/node_modules/popper.js/dist/umd/popper.js"></script>
+<script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<script>
+    $(function() {
+        $("#slider").slider();
+    });
+</script>
+*/
